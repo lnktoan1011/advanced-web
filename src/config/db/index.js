464d10b3f854bd6eff454b90fa8b4f854ab004b0
Toan Le <lnktoan1011@gmail.com>
2021-12-29 22:49:52 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connect() {
 
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/final_project');
+        await mongoose.connect('mongodb+srv://admin:Abc1234@cluster0.wvmvl.mongodb.net/advanced-web?retryWrites=true&w=majority');
 
         console.log("connect successfully!!!");
     } catch (error) {
